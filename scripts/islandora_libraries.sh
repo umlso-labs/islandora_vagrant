900e24d147e19e34f44b0cae5e537a67779cfc51
Phil Redmon <redmonp@missouri.edu>
2018-04-17 16:45:26 -0500

    Fix umlts changes for 7.x-1.9

--- scripts/islandora_libraries.sh
+++ scripts/islandora_libraries.sh
@@ -21,3 +21,19 @@ sudo drush -v -y en islandora_openseadragon
 
 # After last drush call from root user, change cache permissions
 sudo chown -R vagrant:vagrant "$HOME_DIR"/.drush
+
+# Install plupload library v1 
+cd "$DRUPAL_HOME"/sites/all/libraries || exit
+curl -L -sS -o plupload.zip https://github.com/moxiecode/plupload/archive/v1.5.8.zip
+unzip plupload.zip 
+rm plupload.zip 
+mv plupload-* plupload 
+sudo chown -R vagrant:www-data "$DRUPAL_HOME"/sites/all/libraries
+
+# Install fits-0.6.2
+cd /tmp || exit
+curl -sS -o fits.zip http://projects.iq.harvard.edu/files/fits/files/fits-0.6.2.zip
+cd /opt || exit
+sudo unzip "/tmp/fits.zip"
+sudo chown -R vagrant:www-data /opt/fits-0.6.2
+sudo chmod ug+x /opt/fits-0.6.2/fits.sh
--- scripts/post.sh
+++ scripts/post.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+SHARED_DIR=$1
+if [ -f "$SHARED_DIR/configs/variables" ]; then
+  . "$SHARED_DIR"/configs/variables
+fi
+
+# Install features to enforce djatoka url setting, among other things
+cd "$DRUPAL_HOME"/sites/all/modules || exit
+drush @sites -y -u 1 en features
+git clone https://github.com/umlts-labs/islandora_vagrant_features.git
+cd "$DRUPAL_HOME"/sites/mu || exit
+drush -y -u 1 en islandora_vagrant_features
+drush -y fr islandora_vagrant_features
+cd "$DRUPAL_HOME"/sites/umkc || exit
+drush -y -u 1 en islandora_vagrant_features
+drush -y fr islandora_vagrant_features
+cd "$DRUPAL_HOME"/sites/umsl || exit
+drush -y -u 1 en islandora_vagrant_features
+drush -y fr islandora_vagrant_features
+
 # Setup a user for Tomcat Manager
 sed -i '$i<role rolename="admin-gui"/>' /etc/tomcat7/tomcat-users.xml
 sed -i '$i<user username="islandora" password="islandora" roles="manager-gui,admin-gui"/>' /etc/tomcat7/tomcat-users.xml
@@ -7,6 +26,49 @@ service tomcat7 restart
 
 # Set correct permissions on sites/default/files
 chmod -R 775 /var/www/drupal/sites/default/files
+
+# Fix drupal permissions
+# https://www.drupal.org/node/244924
+cd "$DRUPAL_HOME"
+chown -R vagrant:www-data .
+# chmod 750 for all *directories* in drupal root (recursive) 
+find . -type d -exec chmod u=rwx,g=rx,o= '{}' \;
+# chmod 640 for all *files* in drupal root (recursive)
+find . -type f -exec chmod u=rw,g=r,o= '{}' \;
+
+if [ -d /mnt/storage ]; then 
+	#/mnt/storage
+	chown -R vagrant:www-data /mnt/storage
+	chmod 770 /mnt/storage 
+
+	#files dir 
+	chown -R vagrant:www-data /mnt/storage/files
+	chmod 770 /mnt/storage/files 
+	sites_arr=( default lso merlin mu umkc umkclaw umkcscholar umsl ) 
+	for i in "${sites_arr[@]}"
+	do
+		chmod 770 /mnt/storage/files/"$i"
+		for d in /mnt/storage/files/"$i"
+		do
+			find $d -type d -print0 -exec chmod ug=rwx,o= '{}' \;
+			find $d -type f -print0 -exec chmod ug=rw,o= '{}' \;
+		done
+	done
+
+	#private dir 
+	chown -R vagrant:www-data /mnt/storage/private
+	chmod 770 /mnt/storage/private
+	sites_array=( default lso merlin mu umkc umkcscholar umsl ) 
+	for i in "${sites_array[@]}"
+	do
+		chmod 770 /mnt/storage/private/"$i"
+		for d in /mnt/storage/private/"$i"
+		do
+			find $d -type d -print0 -exec chmod ug=rwx,o= '{}' \;
+			find $d -type f -print0 -exec chmod ug=rw,o= '{}' \;
+		done
+	done
+fi
 
 # Allow anonymous & authenticated users to view repository objects
 drush --root=/var/www/drupal role-add-perm "anonymous user" "view fedora repository objects"
